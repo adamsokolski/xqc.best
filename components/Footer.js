674e830103ebb0e4587dde72c4fa016b91c1e952
Adam Sokólski <idKrazu@gmail.com>
2021-12-18 04:46:48 +0100

    ✨ Add link to credits in footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,11 +2,16 @@ import { FooterStyled } from "./styles/FooterStyled";
 import Image from "next/image";
 import styled from "styled-components";
 import Link from "next/link";
+import { FancyLinkStyled } from "./styles/FancyLinkStyled";
 
 const ImageWrapper = styled.div`
   margin: 0 5px;
   display: flex;
   align-items: flex-end;
+`;
+
+const InlineLink = styled.span`
+  margin-left: 5px;
 `;
 
 export default function Footer() {
@@ -22,7 +27,13 @@ export default function Footer() {
           alt="pixel heart"
         />{" "}
       </ImageWrapper>
-      by NiceDevTools <span className="footer-small">in chat</span>
+      by
+      <Link href="/credits">
+        <InlineLink>
+          <FancyLinkStyled>NiceDevTools</FancyLinkStyled>
+        </InlineLink>
+      </Link>
+      <span className="footer-small">in chat</span>
     </FooterStyled>
   );
 }
--- components/styles/FancyLinkStyled.js
+++ components/styles/FancyLinkStyled.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const FancyLinkStyled = styled.a`
+  display: inline-block;
+  width: auto;
   background-image: linear-gradient(90deg, #f06836 0%, #f06836 100%);
   background-size: 0% 2px;
   background-repeat: no-repeat;
