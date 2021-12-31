cb55643341c91f49561f38a0ce3c9c9b6cc9f8fb
Adam Sokólski <idKrazu@gmail.com>
2021-12-31 21:32:22 +0100

    Revert "✨ Add getStaticProps"
    This reverts commit 57bd2e2bd81269279f44287660cfe5cadd1cd14a.

--- pages/list.js
+++ pages/list.js
@@ -1,42 +1,9 @@
 import { xqc2021 } from "../data/xqc2021";
-import { google } from "googleapis";
-import { formatDistanceToNow } from "date-fns";
 
-export async function getStaticProps({ query }) {
-  // Auth
-  const auth = await google.auth.getClient({
-    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-  });
-  const sheets = google.sheets({ version: "v4", auth });
-
-  // Query
-
-  const range = `Sheet2!A2:B2`;
-
-  const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: process.env.SHEET_ID,
-    range,
-  });
-
-  // Result
-
-  const [sum, date] = response.data.values[0];
-  console.log(response.data.values);
-
-  return {
-    props: {
-      sum,
-      date,
-    },
-  };
-}
-
-export default function List({ sum, date }) {
+export default function List() {
   return (
     <div>
       <strong>you shouldn&apos;t be here 🙂</strong>
-      <p>Votes: {sum}</p>
-      <p>Updated {formatDistanceToNow(new Date(date), { addSuffix: true })}</p>
       {xqc2021.categories.map((category) => (
         <div key={category.id}>
           <h2>{category.name}</h2>
